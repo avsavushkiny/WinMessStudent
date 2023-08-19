c2c73ef8356b5988b7e3badd2c1253d636b3c318
Alexander Savushkin <74372739+avsavushkiny@users.noreply.github.com>
2023-08-19 21:51:11 +0300

    Добавьте файлы проекта.

---
+++ Form1.Designer.cs
@@ -0,0 +1,109 @@
+﻿namespace WinMessStudent
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Bisque;
+            this.button1.Location = new System.Drawing.Point(297, 122);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Read";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(13, 12);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox1.Size = new System.Drawing.Size(359, 104);
+            this.textBox1.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.ForeColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.label1.Location = new System.Drawing.Point(12, 127);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(115, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "path to message.txt file";
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.Honeydew;
+            this.button3.Location = new System.Drawing.Point(216, 122);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "to Browser";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.LightBlue;
+            this.ClientSize = new System.Drawing.Size(384, 154);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Winme Student";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+using System.IO.Ports;
+using System.Diagnostics;
+using System.Threading;
+using System.Reflection.Emit;
+
+namespace WinMessStudent
+{
+    public partial class Form1 : Form
+    {
+        string path_to_message_file;
+        string path_to_preferences = "c:\\Winme";
+        string path_to_preferences_file = "c:\\Winme\\preferences.txt";
+        string text;
+
+        private void message_box(string Text, String Title, MessageBoxIcon ICON)
+        {
+            MessageBox.Show(Text, Title,
+                    MessageBoxButtons.OK,
+                    ICON);
+        }
+
+        private void path_to_file()
+        {
+            try
+            {
+                path_to_message_file = File.ReadAllText("C:\\Winme\\preferences.txt");
+                label1.Text = path_to_message_file;
+
+                if (path_to_message_file == "")
+                {
+                    textBox1.Text = "The file preferences.txt is empty! Specify the path to the message file!";
+                    textBox1.Enabled = false;
+
+                    message_box("The file preferences.txt is empty! " +
+                        "\nSpecify the path to the message file!", "Error", MessageBoxIcon.Error);
+
+                    label1.Text = "Empty";
+                }
+            }
+            catch
+            {
+                message_box("Unforeseen error related to the configuration file! " +
+                    "\n Check the location of the preferences.txt file." +
+                    "\n\n" + path_to_preferences_file,
+                    "Error", MessageBoxIcon.Error);
+            }
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                text = File.ReadAllText(path_to_message_file);
+                textBox1.Text = text;
+                Clipboard.SetText(text);
+
+                message_box("Data copied to buffer!", "Information", MessageBoxIcon.Information);
+            }
+            catch
+            {
+                message_box("Message not accepted! Possible reasons:" +
+                    "\n- the path to the message.txt file is not specified" +
+                    "\n- problems accessing the message.txt file",
+                    "Warning", MessageBoxIcon.Warning);
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            if (File.Exists(path_to_preferences_file) == false)
+            {
+                System.IO.Directory.CreateDirectory(path_to_preferences);
+                File.Create(path_to_preferences_file).Close();
+
+                textBox1.Text = "The preferences.txt file has been created! Open the file " + path_to_preferences_file +
+                    ", write the path to the message.txt file. For example: \\\\Server\\D\\message.txt";
+                textBox1.Enabled = false;
+
+                message_box("The preferences.txt file has been created! \n" +
+                    "\n- Open the file " + path_to_preferences_file +
+                    "\n- Write the path to the message.txt file" +
+                    "\n\nFor example:" +
+                    "\n\\\\Server\\D\\message.txt", "Information", MessageBoxIcon.Information);
+            }
+            else
+            {
+                label1.Text = path_to_message_file;
+            }
+
+            path_to_file();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                Process.Start(text);
+            }
+            catch
+            {
+                Process.Start("https://www.google.com/search?q=" + text);
+            }
+        }
+    }
+}
